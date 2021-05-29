9f1a7008b64de53bd2037b5dc0e811882b511f57
abs001 <abhijitkumbhar001@gmail.com>
2021-05-29 22:16:18 +0530

    Added search functionality for products

--- ecomweb/views.py
+++ ecomweb/views.py
@@ -4,4 +4,7 @@ from .models import Products
 
 def index(request):
     product_object = Products.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_object = product_object.filter(title__icontains=item_name)
     return render(request, 'ecomweb/index.html', {"product_object": product_object})
